e79c61cb0266d93aaaf70856733f1a901a25e9e2
Justin Mazzi <hh@mailheist.com>
2010-11-10 11:09:01 -0500

    Setup the library

---
+++ lib/squall.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'json'
+require 'rest_client'
+require 'open-uri'
+
+[:client, :virtual_machine].each do |f|
+  require File.join(File.dirname(__FILE__), 'squall', f.to_s)
+end
+
+module Squall
+ class << self
+    attr_accessor :api_endpoint, :api_user, :api_password
+    def config(api_user, api_password, api_url)
+      @api_user     = api_user
+      @api_password = api_password
+      @api_endpoint = URI.parse(api_url)
+    end
+  end
+end
