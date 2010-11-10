a47d07471ccfeeb3108a90c74ddced5879094361
Justin Mazzi <hh@mailheist.com>
2010-11-10 15:13:10 -0500

    Rework this a bit since the API can sometimes http status codes rest-client thinks are fatal

--- lib/squall/client.rb
+++ lib/squall/client.rb
@@ -1,50 +1,40 @@
 module Squall
   class Client
 
+    attr_reader :successful, :response
+
     def initialize
-      @default_options = {:accept => :json}
+      @default_options = {:accept => :json, :content_type => 'application/json'}
       RestClient.log = STDERR
     end
 
     def get(uri)
-      parse(RestClient.get("#{uri_with_auth}/#{uri}.json", @default_options))
+      parse RestClient.get("#{uri_with_auth}/#{uri}.json", @default_options)
     end
 
     def post(uri, params = {})
-      parse(RestClient.post("#{uri_with_auth}/#{uri}.json", params.to_json, @default_options))
+      RestClient.post("#{uri_with_auth}/#{uri}.json", params.to_json, @default_options)
     end
 
     def put(uri, params = {})
-      RestClient.put("#{uri_with_auth}/#{uri}.json", params.to_json, @default_options)
+      parse RestClient.put("#{uri_with_auth}/#{uri}.json", params.to_json, @default_options)
     end
 
     def delete(uri)
-      RestClient.delete("#{uri_with_auth}/#{uri}.json", @default_options) {  |response, request, result, &block|
-        handle_response(response)
-      }
+      parse RestClient.delete("#{uri_with_auth}/#{uri}.json", @default_options)
     end
 
     private
-
-    def parse(content)
-      JSON.parse(content)
-    end
-
-    def handle_response(response)
-      case response.code
-      when 200
-        return true
-      when 404
-        return false
-      else
-        raise "An unknown error occurred #{response.code}: #{result}"
-      end
-    end
 
     def uri_with_auth
       uri = Squall.api_endpoint
       protocol = uri.port == 443 ? 'https' : 'http'
       "#{protocol}://#{Squall.api_user}:#{Squall.api_password}@#{uri.host}#{uri.path}"
+    end
+
+    def parse(result, code = 200)
+      @response = (result.strip.empty? ? result : JSON.parse(result))
+      @successful = (code == result.code)
     end
 
     def required_options!(required, actual)
