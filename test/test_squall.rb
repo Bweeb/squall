54d9eca946e76f5b4b0bf7c50ea7ea30172580a5
Justin Mazzi <hh@mailheist.com>
2010-11-16 11:54:00 -0500

    Add tests for Client and Squall

--- test/test_squall.rb
+++ test/test_squall.rb
@@ -1,7 +1,12 @@
-require 'helper'
+require File.dirname(__FILE__) + '/helper'
 
 class TestSquall < Test::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  def test_config
+    url = 'http://example.com/onapp'
+    assert_equal 'user', Squall.api_user
+    assert_equal 'stupidpass', Squall.api_password
+    assert_not_nil Squall.api_endpoint
+    assert_equal URI.parse(url), Squall.api_endpoint
   end
 end
