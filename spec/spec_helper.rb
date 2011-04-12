484257eba5499cd8faa2bdab7924f8675e1073aa
Justin Mazzi <hh@mailheist.com>
2011-04-12 12:50:15 -0400

    Look for a ~/.squall.yml for settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require 'rspec'
 require 'vcr'
 require 'squall'
@@ -10,22 +11,30 @@ end
 
 RSpec.configure do |c|
   c.extend VCR::RSpec::Macros
-  # c.before(:each) do
-  # 	pp Squall.config
-  # end
   c.after(:each) do
 		Squall.reset_config
 	end
 end
 
 def default_config
+  yaml = File.join(ENV['HOME'], '.squall.yml')
+  if ENV['SQUALL_LIVE'] && File.exists?(yaml)
+    config = YAML::load_file(yaml)
+    uri  = config['base_uri']
+    user = config['username']
+    pass = config['passwords']
+  else
+    uri  = 'http://www.example.com'
+    user = 'user'
+    pass = 'pass'
+  end
 	Squall.config do |c|
-		c.base_uri 'http://www.example.com'
-		c.username 'user'
-		c.password 'pass'
+		c.base_uri uri
+		c.username user
+		c.password pass
 	end
 end
 
 def requires_attr(attr, &block)
-  expect { block.call }.to raise_error(ArgumentError, /Missing required parameter: #{attr}/i)
+  expect { block.call }.to raise_error(ArgumentError, /Missing required params: #{attr}/i)
 end
