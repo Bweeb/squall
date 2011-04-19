01f1ed03045ca26e4793c6378ef07b891f3ce834
Justin Mazzi <hh@mailheist.com>
2011-04-19 11:27:38 -0400

    Network#edit

--- lib/squall/network.rb
+++ lib/squall/network.rb
@@ -4,5 +4,10 @@ module Squall
       response = request(:get, '/settings/networks.json')
       response.collect { |network| network['network'] }
     end
+
+    def edit(id, options = {})
+      params.accepts(:label, :network_group_id, :vlan, :identifier).validate!(options)
+      response = request(:put, "/settings/networks/#{id}.json", default_params(options))
+    end
   end
 end
--- spec/squall/network_spec.rb
+++ spec/squall/network_spec.rb
@@ -20,4 +20,38 @@ describe Squall::Network do
       network['label'].should == 'Test'
     end
   end
+
+  describe "#edit" do
+    use_vcr_cassette 'network/edit'
+    it "requires an id" do
+      expect { @network.edit }.to raise_error(ArgumentError)
+      @network.success.should be_false
+    end
+
+    it "errors on invalid params" do
+      expect { @network.edit(1, :what => 1) }.to raise_error(ArgumentError, 'Unknown params: what')
+    end
+
+    it "accepts valid params" do
+      @network.edit(1, :label => 'one')
+      @network.success.should be_true
+
+      @network.edit(1, :network_group_id => 1)
+      @network.success.should be_true
+
+      @network.edit(1, :identifier => 'lolzsdfds')
+      @network.success.should be_true
+
+      @network.edit(1, :vlan => 1)
+      @network.success.should be_true
+
+      @network.edit(1, :label => 'two', :vlan => 2, :identifier => 'woah')
+      @network.success.should be_true
+    end
+
+    it "404s on not found" do
+      expect { @network.edit(404) }.to raise_error(Squall::NotFound)
+      @network.success.should be_false
+    end
+  end
 end
