35ae759311aef39f3fbe8206c1bb200cbc363cb7
tfwright <tfwright@gmail.com>
2012-02-10 14:24:55 -0600

    Refactored user#virtual_machines specs, refreshed recordings

--- spec/squall/user_spec.rb
+++ spec/squall/user_spec.rb
@@ -186,20 +186,12 @@ describe Squall::User do
     end
 
     it "404s on not found" do
-      pending "Broken in OnApp" do
-        expect { @user.virtual_machines(500) }.to raise_error(Squall::NotFound)
-      end
+      expect { @user.virtual_machines(404) }.to raise_error(Squall::NotFound)
     end
 
     it "returns the virtual_machines" do
       virtual_machines = @user.virtual_machines(1)
-      virtual_machines.size.should be(2)
-      keys = ["monthly_bandwidth_used", "cpus", "label", "created_at", "operating_system_distro", "cpu_shares",
-              "operating_system", "template_id", "allowed_swap", "local_remote_access_port", "memory", "updated_at",
-              "allow_resize_without_reboot", "recovery_mode", "hypervisor_id", "id", "xen_id", "user_id", "total_disk_size",
-              "booted", "hostname", "template_label", "identifier", "initial_root_password", "min_disk_size",
-              "remote_access_password", "built", "locked", "ip_addresses"]
-      virtual_machines.first.should include(*keys)
+      virtual_machines.should be_an(Array)
     end
   end
   
