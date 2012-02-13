d0513f5a4f0c193e06ef4f8d0821a2987d57680e
tfwright <tfwright@gmail.com>
2012-02-13 11:52:12 -0600

    Added payment#create

--- lib/squall/payment.rb
+++ lib/squall/payment.rb
@@ -7,6 +7,21 @@ module Squall
       response = request(:get, "/users/#{user_id}/payments.json")
       response.collect { |user| user['payment'] }
     end
+    
+    # Create a payment for a user
+    #
+    # ==== Options
+    #
+    # * +options+ - Params for creating the User
+    #
+    # ==== Example
+    #
+    #   create :amount                 => 500,
+    #          :invoice_number         => "01234",
+    def create(user_id, options={})
+      params.required(:amount).accepts(:invoice_number).validate!(options)
+      request(:post, "/users/#{user_id}/payments.json", default_params(options))
+    end
 
   end
 end
--- spec/squall/payment_spec.rb
+++ spec/squall/payment_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Squall::Payment do
   before(:each) do
-    @keys = []
+    @keys = ["amount", "invoice_number"]
     @payment = Squall::Payment.new
-    @valid = {}
+    @valid = {:amount => 500}
   end
 
   describe "#list" do
@@ -22,6 +22,35 @@ describe Squall::Payment do
     it "contains first payment's data" do
       payment = @payment.list(1).first
       payment.should be_a(Hash)
+    end
+  end
+  
+  describe "#create" do
+    use_vcr_cassette "payment/create"
+    it "requires amount" do
+      invalid = @valid.reject{|k,v| k == :amount }
+      requires_attr(:amount) { @payment.create(1, invalid) }
+    end
+    
+    it "allows all optional params" do
+      optional = [:invoice_number]
+      @payment.should_receive(:request).exactly(optional.size).times.and_return Hash.new("payment" => {})
+      optional.each do |param|
+        @payment.create(1, @valid.merge(param => "test"))
+      end
+    end
+    
+    it "raises error on unknown params" do
+      expect { @payment.create(1, @valid.merge(:what => 'what')) }.to raise_error(ArgumentError, 'Unknown params: what')
+    end
+    
+    it "raises an error for an invalid user id" do
+      expect { @payment.create(404, @valid) }.to raise_error(Squall::NotFound)
+    end
+
+    it "creates a payment for a user" do
+      user = @payment.create(1, @valid)
+      @payment.success.should be_true
     end
   end
 
