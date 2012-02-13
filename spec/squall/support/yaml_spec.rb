48e4a120a4b3e99df21eab396ffad5318621dc8a
tfwright <tfwright@gmail.com>
2012-02-13 11:20:55 -0600

    Removed unnecessary test

--- spec/squall/support/yaml_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe "YAML::ENGINE.yamler", :if => YAML.const_defined?(:ENGINE) && !(RUBY_PLATFORM == "java" && RUBY_VERSION == "1.9.2") do
-  it { YAML::ENGINE.yamler.should == 'syck' }
-end
