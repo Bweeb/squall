f02cd23cc7b4354bbf116cdbc3e18571cd00c677
tfwright <tfwright@gmail.com>
2012-02-13 11:38:48 -0600

    Added documentation to user#list

--- lib/squall/payment.rb
+++ lib/squall/payment.rb
@@ -2,6 +2,7 @@ module Squall
   # OnApp Payment
   class Payment < Base
 
+    # Return a list of all payments
     def list(user_id)
       response = request(:get, "/users/#{user_id}/payments.json")
       response.collect { |user| user['payment'] }
